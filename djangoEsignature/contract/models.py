a8f2d4769d11ccb58988381d2ebb98bd1a758dac
amirdev1883 <amirdev1883@gmail.com>
2024-11-30 01:42:48 +0330

    create models contract and emailcontract

--- djangoEsignature/contract/models.py
+++ djangoEsignature/contract/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Contract(models.Model):
+    text = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='contracts')
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f'{self.user.username} - {self.text}'
+    
+    
+class EmailContract(models.Model):
+    contract = models.OneToOneField(Contract, on_delete=models.CASCADE, related_name='emails')
+    fullname = models.CharField(max_length=100)
+
+
--- djangoEsignature/djangoEsignature/settings.py
+++ djangoEsignature/djangoEsignature/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'accounts.apps.AccountsConfig',
+    'contract.apps.ContractConfig',
 
     'rest_framework',
 

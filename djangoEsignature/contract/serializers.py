d311ac331d9fe87f4e8621ab157924d27b8a200f
amirdev1883 <amirdev1883@gmail.com>
2024-12-09 19:18:26 +0330

    change contract serializers.py

--- djangoEsignature/contract/serializers.py
+++ djangoEsignature/contract/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
-from .models import UsersInfo
 
-class UsersInfoSerializer(serializers.ModelSerializer):
-
-    class Meta :
-        model = UsersInfo
-        fields = ('fullname', 'email',)
-    
+class UsersInfoSerializer(serializers.Serializer):
+    text = serializers.CharField(
+        max_length=500, 
+        allow_blank=False 
+    )
+    emails = serializers.ListField(
+        child=serializers.EmailField(),
+        allow_empty=False,  
+        min_length=1,       
+    )
+    fullnames = serializers.ListField(
+        child=serializers.CharField(),
+        allow_empty=False,  
+        min_length=1,       
+    )
